67fc2b53e9c157fad500be5754132cd14491c605
unknown <pratikraval2603@gmail.com>
2021-11-23 00:31:50 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<Navbar/>
+<Login/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Navbar from './components/Navbar.vue';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import Login from './components/Login.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar,
+    Login,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
